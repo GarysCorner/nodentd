bb4465923c762bd0ebdc546f3ac3ff7bd47a63ca
Gary Bezet <nodentd@GarysCorner.NET>
2015-05-07 20:01:12 -0500

    Added block_sysuser config option to realidentd_userid to prevent system user ids from being passed into the system for security reasons.

--- config.sample.js
+++ config.sample.js
@@ -13,6 +13,16 @@ exports.getconfig = {
 		nameproviders: [ 'use_ip_port', 'use_ip', 'use_port',  'random_fromlist' /* , 'defaultname' */ ],  //list of resolvers in a typical stack
 
 		provider: {  //configuration options for the providers you can have configurations options for all providers, but only providers listed in "nameproviders" will be used
+			
+			realidentd_userid: {  //options for the realidentd_userid this plugin is used even if not listed for any plugin that provides actual identity (realidentd_*)
+				/*
+					block_sysusers: true,		//do not provider userids under 1000 because they are system users.
+					
+					
+				/*
+			},
+			
+			
 			defaultname: {    //options for default name provider.  The default name provider will only return the username specified bellow, and will not fail under any conditions as long as the username is set.  Default name will probably not be used in all but the dumbest identd servers
 				/* username: 'DefaultUser' */
 			},
@@ -47,7 +57,9 @@ exports.getconfig = {
 
 			random_fromlist: {  //options for the random_fromlist name provider.  The randome_fromlist name provider should always return a random name from the list.  The list should be one name to a line, with no blank lines, and lines should be \n NOT \r\n  (I'll fix that I promise)
 				file: './nameproviders/commonusernames.txt'
-			}
+			},
+			
+			
 		}
 
 
--- nameproviders/realidentd_userid.js
+++ nameproviders/realidentd_userid.js
@@ -8,22 +8,41 @@ var fs = require('fs');
 var procfile = '/proc/net/tcp';
 
 
+var realconf;
+
 exports.init = function() {
 
-	
 
+	
 	if( process.platform !== 'linux' ) {  //Warn if we aren't using linux
-		log.dlog('Warning!  Real identd is only test on linux but the os reports as "', process.platform, '"');
+		log.dlog('realidentd_userid:  Warning Real identd is only test on linux but the os reports as "', process.platform, '"');
 	}
 
 	try{
 		var testdata = fs.readFileSync(procfile, { encoding: 'ascii' }); 
 	} catch( err ) {
-		log.log('Cloud not read from "', procfile, '": ', err.message);
+		log.log('realidentd_userid:  Cloud not read from "', procfile, '": ', err.message);
 		return false;
 	}
+	
 
+	if( config.provider.realidentd_userid === undefined ) {  //check if the configuration has been set
+		log.log('realidentd_userid:  Warning config.provider.realidentd_userid is not set, it a good idea to configure it.');
+		config.provider.realidentd_userid = { };
+	}
+	
+	
+	if( config.provider.realidentd_userid.block_sysusers === undefined ) { //check if block_sysusers is set and set default
+	
+		log.log('realidentd_userid:  blocksysusers not set, defaulting to true for security.');
+		config.provider.realidentd_userid.block_sysusers = true;
+	
+	}
 
+	realconf = config.provider.realidentd_userid;
+
+	
+	
 	return true;
 
 }
@@ -63,6 +82,9 @@ exports.providename = function(result, socket, callback) {
 		for( i=1; i<data.length-1; i++ ) {  //the first line is headers and the last is after a return
 			
 			row = data[i].split(/\s+/);		//split data on columns
+
+			if( config.provider.realidentd_userid.block_sysusers && ( parseInt(row[8]) < 1000 )) {continue;} //check if userid is <1000 =sysuser
+
 			useridlist.push( row[8] );
 			portlist.push( row[2].match(portregex)[0].concat(row[3].match(portregex)[0]));
 			
